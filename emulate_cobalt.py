3b5c9f6d88ce70900dfac5fc19d871e4dc94113d
fairwinds0099 <fairwinds0099@gmail.com>
2025-04-22 18:34:43 -0400

    updated cobalt strike

--- emulate_cobalt.py
+++ emulate_cobalt.py
@@ -3,7 +3,7 @@ from scapy.all import *
 target_ip = "192.168.28.0/24"
 
 # Build Ethernet frame manually if needed
-eth = Ether(dst=mac)
+eth = Ether()
 ip = IP(src="139.155.190.117", dst=target_ip)
 udp = UDP(dport=4343)
 payload = Raw(load="CobaltStrikePing")
