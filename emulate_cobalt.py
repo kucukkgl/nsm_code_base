ec2e1e79d2ff384b74f0645fbdba23cb04690e3b
fairwinds0099 <fairwinds0099@gmail.com>
2025-04-22 18:33:44 -0400

    updated cobalt strike

--- emulate_cobalt.py
+++ emulate_cobalt.py
@@ -2,14 +2,6 @@ from scapy.all import *
 
 target_ip = "192.168.28.0/24"
 
-# Try resolving the MAC
-mac = getmacbyip(target_ip)
-if mac is None:
-    print("Could not resolve MAC ")
-    exit()
-
-print("Resolved MAC:")
-
 # Build Ethernet frame manually if needed
 eth = Ether(dst=mac)
 ip = IP(src="139.155.190.117", dst=target_ip)
